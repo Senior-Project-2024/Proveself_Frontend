b490a9c60051033289fc7cfd9810334b0479301f
Sorathorn Kaewchotchuangkul <sarathorn.16@gmail.com>
2024-04-29 23:11:37 +0700

    del config next

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-  serverRuntimeConfig: {
-    BACKEND_HOST: process.env.BACKEND_HOST,
-  },
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_BACKEND_HOST: process.env.NEXT_PUBLIC_BACKEND_HOST,
-  },
 };
 
 module.exports = nextConfig;
