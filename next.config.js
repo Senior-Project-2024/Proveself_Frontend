2f5979484fc1a454825830bd01c82c8f9bb80191
Sorathorn Kaewchotchuangkul <sarathorn.16@gmail.com>
2024-04-29 23:02:34 +0700

    change next config againn....

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  serverRuntimeConfig: {
+    BACKEND_HOST: process.env.BACKEND_HOST,
+  },
   publicRuntimeConfig: {
     NEXT_PUBLIC_BACKEND_HOST: process.env.NEXT_PUBLIC_BACKEND_HOST,
   },
