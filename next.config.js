b9c9d9239823b26f54b5f577a652afe1d7229477
Sorathorn Kaewchotchuangkul <sarathorn.16@gmail.com>
2024-04-29 22:59:14 +0700

    change next config

--- next.config.js
+++ next.config.js
@@ -5,11 +5,8 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-  serverRuntimeConfig: {
-    API_URL: process.env.API_URL,
-  },
   publicRuntimeConfig: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_BACKEND_HOST: process.env.NEXT_PUBLIC_BACKEND_HOST,
   },
 };
 
